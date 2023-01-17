54aa75c1c3d6e93dd3f66edabdfd6b09dddc62df
garganshu <garganshu96@gmail.com>
2023-01-17 16:05:26 +0530

    add: deployment script and smart contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,4 +3,10 @@ require('dotenv').config();
 
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    goerli: {
+        url: process.env.TESTNET_ALCHEMY_URL,
+        accounts:[process.env.TEST_PRIVATE_KEY]
+    }
+  }
 };
